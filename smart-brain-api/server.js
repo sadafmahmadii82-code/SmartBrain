a1013deb26e7e73aba4cb92c2f5e5f8af2e8b7de
sadafmahmadii82-code <sadafm.ahmadii82@gmail.com>
2025-10-15 18:51:30 +0330

    Initial commit

---
+++ smart-brain-api/server.js
@@ -0,0 +1,105 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors');
+const knex = require('knex');
+
+
+const db = knex({
+	client:'pg',
+	connection:{
+		host :'127.0.0.1',
+		port : 5432 ,
+		user :'postgres',
+		password :'46761382',
+		database :'smartbrain'
+	}
+});
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/' , (req,res)=>{
+	res.json(database.users);
+})
+
+app.post('/signin' , (req,res)=>{
+	db.select('email','hash').from('login')
+	.where('email' , '=' , req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password , data[0].hash);
+		if(isValid) {
+			return db.select('*').from('users')
+			.where('email' , '=' , req.body.email)
+			.then(user => {
+				res.json(user[0]);
+			})
+			.catch(err => res.status(404).json('unable to find user'))
+		}else {
+			res.status(400).json('wrong password or email')
+		}
+	})
+	.catch(err => res.status(404).json('an err happend'))
+})
+
+app.post('/register' , (req,res)=>{
+	const {email,name,password} = req.body;
+	const hash = bcrypt.hashSync(password);
+	
+	db.transaction(trx => {
+		trx.insert({
+			hash : hash,
+			email : email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+				.returning('*')
+				.insert({
+					email:email,
+					name:name,
+					joined:new Date()
+			    }).then(user => {
+					    res.json(user[0])
+				    })
+			   })
+				.then(trx.commit)
+				.catch(trx.rollback)
+	       })
+			.catch(err => res.status(400).json('unable to join'))
+     })
+
+app.get('/profile/:id', (req,res)=>{
+	const {id} = req.params;
+	db.select('*').from('users').where({
+		id:id
+	})
+	.then(user => {
+		if(user.length){
+			res.json(user[0]);
+		} else {
+			res.status(404).json('not found')
+		}
+		
+	})
+	.catch(err => res.status(404).json('an err happend'))
+})
+
+app.put('/image', (req,res)=> {
+	const {id} = req.body;
+	db('users')
+	.where('id' , '=' , id)
+	.increment('entries' , 1)
+	.returning('entries')
+	.then(entries => {
+		res.status(400).json((entries[0].entries));
+	})
+})
+
+app.listen(3000 , ()=>{
+	console.log('app is running');
+})
