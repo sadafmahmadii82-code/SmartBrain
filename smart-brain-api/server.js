b76338ae830c34164d48b0af3cc290f6c9f90ce9
sadafmahmadii82-code <sadafm.ahmadii82@gmail.com>
2025-10-19 19:01:08 +0330

    done

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -12,7 +12,7 @@ const db = knex({
 		port :parseInt (process.env.DB_PORT) ,
 		user :process.env.DB_USER,
 		password :process.env.DB_PASSWORD,
-		name :process.env.DB_NAME
+		database :process.env.DB_NAME
 	}
 });
 
